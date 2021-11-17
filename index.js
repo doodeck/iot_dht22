7195d3754c0aa3c9b0dcda681abbaca8dda676ff
doodeck <8515450+doodeck@users.noreply.github.com>
2021-11-17 19:07:15 +0100

    port from env var (heroku)

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 5000;
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   res.send('hello world')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
 })
