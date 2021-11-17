959e22724ebc3a9d16dcd7ef5c3beb414aad9453
doodeck <8515450+doodeck@users.noreply.github.com>
2021-11-17 20:01:51 +0100

    logging access to command line

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # iot_dht22
 backend for esp32_dht22 project
+
+## Deployment on heroku (Ubuntu)
+
+```
+sudo snap install heroku --classic
+heroku login --browser=chromium
+heroku apps:create --region eu iot-dht22
+git push heroku main
+heroku logs --tail
+```
--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const PORT = process.env.PORT || 5000;
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('hello world')
+    console.log('received connection from: ', req.ip);
+    console.log('  Parameters: ', req.params);
+    console.log('  Query: ', req.query);
+    res.send('hello world')
 })
 
 app.listen(PORT, () => {
