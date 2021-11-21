3ae60e261fb08973560551d75adbca9579092172
doodeck <8515450+doodeck@users.noreply.github.com>
2021-11-21 16:04:56 +0100

    changed the authentication configuration - env variable and -env file

--- authentication.js
+++ authentication.js
@@ -1,11 +1,16 @@
 // authentication.js
 
-const auth = require('./credentials.js')
+// const auth = require('./credentials.js')
+
+// env variable : komma separated values, eg. AUTH_IDS="************,************"
+var auth_ids = process.env.AUTH_IDS.split(","); // [ "************", "************" ]
+// MAC addresses (without colon) which are allowed to connect, e.g. "12ab34cd56ef"
 
 module.exports = function (req, res, next) {
     // console.log(req)
+    console.log('auth ids:', auth_ids);
 
-    const found = auth.ids.find(id => id === req.query.id)
+    const found = auth_ids.find(id => id === req.query.id)
     if (typeof found !== 'undefined' && found.length > 0) {
         next()
     } else {
