c455801dcdbdfb7bbc89787fcf974931cef1cfde
doodeck <8515450+doodeck@users.noreply.github.com>
2021-11-27 13:51:02 +0100

    renamed the table to the productive one

--- db/migrate-0_0_1-0_0_2.sql
+++ db/migrate-0_0_1-0_0_2.sql
@@ -8,18 +8,19 @@ introduced:
     id_key INTEGER REFERENCES credentials_ids(key),
 */
 
--- to test in productive DB create a temporary table
+/* to test in productive DB create a temporary table
 create table measurements_n as (select * from measurements LIMIT 50);
 ALTER TABLE measurements_n ALTER COLUMN ts SET DEFAULT now();
+*/
 
-ALTER TABLE measurements_n
+ALTER TABLE measurements
     ADD COLUMN id_key INTEGER;
 
-UPDATE measurements_n m SET id_key = (SELECT key FROM credentials_ids c WHERE c.id = m.id);
+UPDATE measurements m SET id_key = (SELECT key FROM credentials_ids c WHERE c.id = m.id);
 
-ALTER TABLE measurements_n
-    ADD CONSTRAINT "measurements__a_id_key_fkey" FOREIGN KEY (id_key) REFERENCES credentials_ids(key);
+ALTER TABLE measurements
+    ADD CONSTRAINT "measurements_id_key_fkey" FOREIGN KEY (id_key) REFERENCES credentials_ids(key);
 
-ALTER TABLE measurements_n
+ALTER TABLE measurements
     DROP COLUMN id;
 
