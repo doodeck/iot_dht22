bea2b343eff7668084f3a4b3504dbeb16ec0eca1
doodeck <8515450+doodeck@users.noreply.github.com>
2021-12-23 22:55:29 +0100

    added hints on backing up Heroku DB

--- db/truncate.sql
+++ db/truncate.sql
@@ -1,7 +1,10 @@
 -- truncate.sql
 
 -- use when you are hard pressed for DB storage, e.g. free tier of heroku
--- you should probably dump the tables before deleting them
+-- you should probably dump the tables before deleting them, e.g.
+-- for i in measurements weather ; do heroku pg:psql -c "\copy (SELECT * FROM ${i} order by ts) TO latest_${i}.csv CSV DELIMITER ','" ; done
+-- heroku pg:backups:capture
+-- heroku pg:backups:download b006
 
 -- DELETE order matters because of foreign keys
 DELETE FROM measurements;
